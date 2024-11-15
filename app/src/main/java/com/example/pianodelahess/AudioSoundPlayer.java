e079aa2bfaa4263aa0b13b4b8924641d26055809
RaibiJamila <nizar.yassine07@gmail.com>
2024-11-15 15:19:29 +0100

    Changement de lecteur de son : passage de AudioTrack à MediaPlayer. Test OK.

--- app/src/main/java/com/example/pianodelahess/AudioSoundPlayer.java
+++ app/src/main/java/com/example/pianodelahess/AudioSoundPlayer.java
@@ -1,122 +1,91 @@
 package com.example.pianodelahess;
 
 import android.content.Context;
-import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
+import android.media.MediaPlayer;
 import android.util.SparseArray;
-
-import java.io.InputStream;
 
 public class AudioSoundPlayer {
 
-    private SparseArray<PlayThread> threadMap = null;
+    private SparseArray<MediaPlayer> playerMap = new SparseArray<>();
     private Context context;
-    private static final SparseArray<String> SOUND_MAP = new SparseArray<>();
-    public static final int MAX_VOLUME = 100, CURRENT_VOLUME = 90;
 
+    // Map des noms de fichiers associés aux notes
+    private static final SparseArray<String> SOUND_MAP = new SparseArray<>();
     static {
-        // white keys sounds
-        SOUND_MAP.put(1, "note_do");
-        SOUND_MAP.put(2, "note_re");
-        SOUND_MAP.put(3, "note_mi");
-        SOUND_MAP.put(4, "note_fa");
-        SOUND_MAP.put(5, "note_sol");
-        SOUND_MAP.put(6, "note_la");
-        SOUND_MAP.put(7, "note_si");
-        SOUND_MAP.put(8, "second_do");
-        SOUND_MAP.put(9, "second_re");
-        SOUND_MAP.put(10, "second_mi");
-        SOUND_MAP.put(11, "second_fa");
-        SOUND_MAP.put(12, "second_sol");
-        SOUND_MAP.put(13, "second_la");
-        SOUND_MAP.put(14, "second_si");
-        // black keys sounds
-        SOUND_MAP.put(15, "do_dies");
-        SOUND_MAP.put(16, "re_dies");
-        SOUND_MAP.put(17, "fa_dies");
-        SOUND_MAP.put(18, "sol_dies");
-        SOUND_MAP.put(19, "la_dies");
-        SOUND_MAP.put(20, "second_dies_do");
-        SOUND_MAP.put(21, "second_dies_re");
-        SOUND_MAP.put(22, "second_dies_fa");
-        SOUND_MAP.put(23, "second_dies_sol");
-        SOUND_MAP.put(24, "second_dies_la");
+        SOUND_MAP.put(1, "note_do.mp3");
+        SOUND_MAP.put(2, "note_re.mp3");
+        SOUND_MAP.put(3, "note_mi.mp3");
+        SOUND_MAP.put(4, "note_fa.mp3");
+        SOUND_MAP.put(5, "note_sol.mp3");
+        SOUND_MAP.put(6, "note_la.mp3");
+        SOUND_MAP.put(7, "note_si.mp3");
+        SOUND_MAP.put(8, "second_do.mp3");
+        SOUND_MAP.put(9, "second_re.mp3");
+        SOUND_MAP.put(10, "second_mi.mp3");
+        SOUND_MAP.put(11, "second_fa.mp3");
+        SOUND_MAP.put(12, "second_sol.mp3");
+        SOUND_MAP.put(13, "second_la.mp3");
+        SOUND_MAP.put(14, "second_si.mp3");
+        SOUND_MAP.put(15, "do_dies.mp3");
+        SOUND_MAP.put(16, "re_dies.mp3");
+        SOUND_MAP.put(17, "fa_dies.mp3");
+        SOUND_MAP.put(18, "sol_dies.mp3");
+        SOUND_MAP.put(19, "la_dies.mp3");
+        SOUND_MAP.put(20, "second_dies_do.mp3");
+        SOUND_MAP.put(21, "second_dies_re.mp3");
+        SOUND_MAP.put(22, "second_dies_fa.mp3");
+        SOUND_MAP.put(23, "second_dies_sol.mp3");
+        SOUND_MAP.put(24, "second_dies_la.mp3");
     }
 
     public AudioSoundPlayer(Context context) {
         this.context = context;
-        threadMap = new SparseArray<>();
     }
 
+    // Joue une note
     public void playNote(int note) {
-        if (!isNotePlaying(note)) {
-            PlayThread thread = new PlayThread(note);
-            thread.start();
-            threadMap.put(note, thread);
-        }
-    }
+        // Vérifie si une instance MediaPlayer pour cette note existe déjà
+        if (playerMap.get(note) == null) {
+            String fileName = SOUND_MAP.get(note);
+            if (fileName != null) {
+                try {
+                    MediaPlayer mediaPlayer = new MediaPlayer();
+                    // Prépare le MediaPlayer avec le fichier dans le dossier assets
+                    mediaPlayer.setDataSource(context.getAssets().openFd(fileName).getFileDescriptor(),
+                            context.getAssets().openFd(fileName).getStartOffset(),
+                            context.getAssets().openFd(fileName).getLength());
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
 
-    public void stopNote(int note) {
-        PlayThread thread = threadMap.get(note);
+                    // Définir un Listener pour libérer la ressource après lecture
+                    mediaPlayer.setOnCompletionListener(mp -> {
+                        mp.release();
+                        playerMap.remove(note); // Supprime le MediaPlayer de la map après usage
+                    });
 
-        if (thread != null) {
-            threadMap.remove(note);
-        }
-    }
+                    // Sauvegarde l'instance pour référence
+                    playerMap.put(note, mediaPlayer);
 
-    public boolean isNotePlaying(int note) {
-        return threadMap.get(note) != null;
-    }
-
-    private class PlayThread extends Thread {
-        int note;
-        AudioTrack audioTrack;
-
-        public PlayThread(int note) {
-            this.note = note;
-        }
-
-        @Override
-        public void run() {
-            try {
-                String path = SOUND_MAP.get(note) + ".mp3";
-                AssetManager assetManager = context.getAssets();
-                AssetFileDescriptor ad = assetManager.openFd(path);
-                long fileSize = ad.getLength();
-                int bufferSize = 4096;
-                byte[] buffer = new byte[bufferSize];
-
-                audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_MONO,
-                        AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
-
-                float logVolume = (float) (1 - (Math.log(MAX_VOLUME - CURRENT_VOLUME) / Math.log(MAX_VOLUME)));
-                audioTrack.setStereoVolume(logVolume, logVolume);
-
-                audioTrack.play();
-                InputStream audioStream = null;
-                int headerOffset = 0x2C; long bytesWritten = 0; int bytesRead = 0;
-
-                audioStream = assetManager.open(path);
-                audioStream.read(buffer, 0, headerOffset);
-
-                while (bytesWritten < fileSize - headerOffset) {
-                    bytesRead = audioStream.read(buffer, 0, bufferSize);
-                    bytesWritten += audioTrack.write(buffer, 0, bytesRead);
-                }
-
-                audioTrack.stop();
-                audioTrack.release();
-
-            } catch (Exception e) {
-            } finally {
-                if (audioTrack != null) {
-                    audioTrack.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         }
     }
 
+    // Arrête une note si elle est en cours
+    public void stopNote(int note) {
+        MediaPlayer mediaPlayer = playerMap.get(note);
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            playerMap.remove(note);
+        }
+    }
+
+    // Vérifie si une note est en cours de lecture
+    public boolean isNotePlaying(int note) {
+        MediaPlayer mediaPlayer = playerMap.get(note);
+        return mediaPlayer != null && mediaPlayer.isPlaying();
+    }
 }
